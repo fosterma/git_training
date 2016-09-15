f97f6c9293f4bc3339ac8b917b334e3db970fb8e
Mary Lou Foster <mary-lou.foster@mpac.ca>
2016-09-15 10:17:34 -0400

    Reverted the terrible changes from the last commit

--- MPAC.R
+++ MPAC.R
@@ -1,15 +1,15 @@
 # Frequency
 #Compare Means
-#Clkhsdflkhapoitensfross Tabs
-#Validateaslkfjsdlkn Variables
+#Cross Tabs
+#Validate Variables
 #Aggretate by ID
 # Read in a text file and make sure that the first column is character
-# http:asldkfjaskt nlekhsadf//stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
 column_classes=c("character","numeric","character")
-data<-rasdflknasdlkfe;oieead.csv("validate-vars.txt", colClasses=column_classes)
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
 
 # Frequency
-#Cosdflnastoidvncmpare Means
+#Compare Means
 #Cross Tabs
 #Validate Variables
 #Aggretate by ID
